dd77c1b1f82c5282f7fce7a275d1ac8c1a4ca9b6
Stevenson, John [ICG-IT] <js92523@imceu.eu.ssmb.com>
2017-10-23 17:10:34 +0100

    Add a simple test to see test results in TeamCity
    An XML report processing Build Feature has been added to Hello Gradle Wrapper
    build to show results of junit tests,

--- test/hello_gradle/core_test.clj
+++ test/hello_gradle/core_test.clj
@@ -5,3 +5,9 @@
 (deftest a-test
   (testing "I am a very simple passing test to see if the test framework is working"
     (is (= 1 1))))
+
+
+(deftest simple-test
+  (testing "I am another simple test to see what test results look like in TeamCity"
+    (is (= '(1 2 3 4) [1 2 3 4]))))
+

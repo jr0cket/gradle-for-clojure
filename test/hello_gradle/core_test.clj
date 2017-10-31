6d1593a02a77769c51f15fd75d4efaf783e6d9f4
John Stevenson <js92523@imceu.eu.ssmb.com>
2017-10-31 15:54:16 +0000

    Added tests back, fixing missing test names
    Tests with missing names causes the TeamCity build to fail and return an exit
    code of 255

--- test/hello_gradle/core_test.clj
+++ test/hello_gradle/core_test.clj
@@ -6,3 +6,22 @@
   (testing "I am a very simple passing test to see if the test framework is working"
     (is (= 1 1))))
 
+(deftest simple-test
+  (testing "I am another simple test to see what test results look like in TeamCity"
+    (is (= '(1 2 3 4) [1 2 3 4]))))
+
+(deftest fixed-failing-test
+  (testing "Fixed failing test - add more tests and see the results in TeamCity"
+    (is (= "I am not a keyword" "I am not a keyword"))))
+
+(deftest clojure-skills-test
+  (testing "More tests to evaluate my clojure skills"
+    (is (not (= [1 2 3] {:a 1 :b 2 :c 3})))))
+
+(deftest trigger-builds-test
+  (testing "triggering of builds in teamcity from bitbucket commits"
+    (is (= true true))))
+
+(deftest vcs-trigger-builds-test
+  (testing "needed to enable the VCS trigger, which is still required even though we are pushing from our bitbucket webhoot"
+    (is (= 42 42))))
